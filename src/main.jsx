0ddf019b60aa4c8f41ede96ac464a398e68dcbfb
rajlina <rajliabdu@gmail.com>
2026-07-06 16:11:16 +0530

    git commit updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 // import { StrictMode } from 'react'
 import {BrowserRouter}  from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -8,9 +9,11 @@ import ProductContextProvider from './context/ProductContex.jsx';
 
 createRoot(document.getElementById('root')).render(
  <BrowserRouter>
+ 
  <ProductContextProvider>
  
     <App />
     </ProductContextProvider>
   </ BrowserRouter>,
+  
 )
--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-})
+   base: "/Ecommerse-react-project/",
+});
